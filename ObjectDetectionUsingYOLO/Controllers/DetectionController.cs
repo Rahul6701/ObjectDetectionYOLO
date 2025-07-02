11ff687a7583f3a47574bd22b6032164df9ad870
Rahul <rahul.elgoss@gmail.com>
2025-07-02 14:24:27 +0530

    Add Files

---
+++ ObjectDetectionUsingYOLO/Controllers/DetectionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ObjectDetectionUsingYOLO.Controllers
+{
+    public class DetectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult upload()
+        {
+            return View();
+        }
+    }
+}
---
+++ ObjectDetectionUsingYOLO/Models/DetectionInput.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectDetectionUsingYOLO.Models
+{
+    public class DetectionInput
+    {
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+        public float[] Box { get; set; } // [x1, y1, x2, y2]
+
+    }
+}
