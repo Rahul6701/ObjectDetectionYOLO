b05b03cda26d771ad58b40b3cf7104345fd2665d
Rahul <rahul.elgoss@gmail.com>
2025-07-02 14:12:46 +0530

    Add project files.

---
+++ ObjectDetectionUsingYOLO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObjectDetectionUsingYOLO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
